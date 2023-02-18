f1eb07dba6a08f8028290e325216be4938c8996d
YasinSefa15 <yasekirman@gmail.com>
2023-02-18 03:55:07 +0300

    category listing

--- controllers/category_controller.js
+++ controllers/category_controller.js
@@ -9,9 +9,31 @@ const {validate_or_throw_error} = require("../helpers/validation_helper")
 const Joi = require("joi")
 const category = require("../models/category")
 
-exports.read = (req, res) => {
-    console.log("category read")
-    successful_read([], res)
+exports.read = async (req, res) => {
+    const category_result = await category.all({
+        column_names: ['id', 'parent_id', 'title', 'slug']
+    })
+
+    //created an object key is id
+    let categories_list = {}
+
+    Object.keys(category_result).forEach(key => {
+        const parsed = JSON.parse(JSON.stringify(category_result[key]))
+        parsed['children'] = {}
+        if (parsed.parent_id === null) {
+            categories_list[parsed.id] = parsed
+        } else {
+            const parent = categories_list[parsed.parent_id]
+            const child = parsed
+            categories_list[parsed.id] = child
+            do {
+                parent.children[child.id] = child
+            } while (parent === undefined)
+        }
+    })
+
+
+    successful_read(categories_list, res, "Categories listed")
 }
 
 exports.view = async (req, res) => {
--- models/category.js
+++ models/category.js
@@ -5,6 +5,24 @@ const parse_column_names = (column_names) => {
     return column_names === undefined ? "*" : column_names.join(", ").toString()
 }
 
+exports.all = async (input) => {
+    let sql = `SELECT ${parse_column_names(input.column_names)} FROM categories`
+
+    return new Promise((resolve, reject) => {
+        connection.query(sql, (err, result) => {
+            if (err) {
+                logger.error(err)
+                reject(err)
+            } else {
+                resolve(result)
+            }
+        })
+    })
+}
+
+
+
+
 exports.findOneBy = async (input) => {
     let sql = `SELECT ${parse_column_names(input.column_names)} FROM categories where ${input.column} = '${input.value}' LIMIT 1`
 
